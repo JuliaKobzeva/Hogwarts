0a393bd132788e9168f1279a9278cbb1be9b9302
Юля Кобзева <selemina@mail.ru>
2023-01-20 21:38:46 +0300

    дз

--- src/main/java/skypro/hogwarts/repository/FacultyRepository.java
+++ src/main/java/skypro/hogwarts/repository/FacultyRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import skypro.hogwarts.model.Faculty;
 
 public interface FacultyRepository extends JpaRepository <Faculty, Long>{
-    Faculty findByName(String name);
+
 }
